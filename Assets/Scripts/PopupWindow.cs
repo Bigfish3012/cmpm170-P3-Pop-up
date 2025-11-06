ab7409311df1901e38996a382d9850e6f538e14b
Marcus <mstierne@ucsc.edu>
2025-11-05 22:00:58 -0600

    close button changes

--- Assets/Scripts/PopupWindow.cs
+++ Assets/Scripts/PopupWindow.cs
@@ -17,6 +17,35 @@ public class PopupWindow : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        RandomizeCloseButtonCorner();
+    }
+
+    private void RandomizeCloseButtonCorner()
+    {
+        if (closeButton == null) return;
+        RectTransform closeRect = closeButton.GetComponent<RectTransform>();
+        int corner = Random.Range(0, 4);
+        switch (corner)
+        {
+            case 0: //Top Left
+                closeRect.anchorMin = new Vector2(0, 1);
+                closeRect.anchorMax = new Vector2(0, 1);
+                closeRect.pivot = new Vector2(0.5f, 0.5f);
+                closeRect.anchoredPosition = new Vector2(22, -22);
+                break;
+
+            case 1: //Top Right
+                closeRect.anchorMin = new Vector2(1, 1);
+                closeRect.anchorMax = new Vector2(1, 1);
+                closeRect.pivot = new Vector2(0.5f, 0.5f);
+                closeRect.anchoredPosition = new Vector2(-22, -22);
+                break;
+        }
+        closeRect.anchoredPosition += new Vector2(0, 3); //Add offset to fix button height placement
+    }
+
     public void Close()
     {
         if (Score.Instance != null)
